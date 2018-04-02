cc4c7bc44d824878d9b0e3067edbe0e12031f33b
chris <chrisshields88@gmail.com>
2018-04-02 07:12:23 -0400

    Update
    added new map coordinates

--- vue-mapplication/src/components/Map.vue
+++ vue-mapplication/src/components/Map.vue
@@ -12,16 +12,64 @@ export default {
       mapName: this.name + "-map",
       markerCoordinates: [
         {
-          latitude: 41.919281,
-          longitude: -83.339478
+          name: "Founders Brewing Co.",
+          latitude: 42.9584,
+          longitude: -85.6742,
+          url: "http://www.foundersbrewing.com"
         },
         {
-          latitude: 41.956363,
-          longitude: -83.658961
+          name: "Bells Brewery",
+          latitude: 42.28511,
+          longitude: -85.449972,
+          url: "http://www.bellsbeer.com"
         },
         {
-          latitude: 41.809727,
-          longitude: -83.447003
+          name: "New Holland Brewing Co.",
+          latitude: 42.790067,
+          longitude: -86.104204,
+          url: "http://www.newhollandbrew.com"
+        },
+        {
+          name: "Shorts Brewing Co.",
+          latitude: 44.976315,
+          longitude: -85.210035,
+          url: "http://www.shortsbrewing.com"
+        },
+        {
+          name: "Dark Horse Brewery",
+          latitude: 42.26626,
+          longitude: -84.963444,
+          url: "http://www.darkhorsebrewery.com"
+        },
+        {
+          name: "Kuhnhenn Brewing Company",
+          latitude: 42.527963,
+          longitude: -83.046753,
+          url: "http://www.kbrewery.com"
+        },
+        {
+          name: "Jolly Pumpkin Artisan Ales",
+          latitude: 42.323838,
+          longitude: -83.878447,
+          url: "http://www.jollypumpkin.com"
+        },
+        {
+          name: "Arcadia Brewing Co.",
+          latitude: 42.295278,
+          longitude: -85.570512,
+          url: "http://www.arcadiaales.com"
+        },
+        {
+          name: "Detroit Beer Company",
+          latitude: 42.336136,
+          longitude: -83.048882,
+          url: "http://www.detroitbeerco.com"
+        },
+        {
+          name: "North Peak Brewing Company",
+          latitude: 44.765242,
+          longitude: -85.628562,
+          url: "http://www.northpeak.net"
         }
       ]
     };
