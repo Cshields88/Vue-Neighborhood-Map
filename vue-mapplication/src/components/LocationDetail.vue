6c5351fa06813bdd67c8dcffb53c836b13c01a63
chris <chrisshields88@gmail.com>
2018-04-09 07:05:13 -0400

    update

--- vue-mapplication/src/components/LocationDetail.vue
+++ vue-mapplication/src/components/LocationDetail.vue
@@ -1,24 +1,19 @@
 <template>
  <div class="details">
-   <button type="submit">Add Title</button>
-      <h1>{{location.name}}</h1>
-      <li v-for="location in locations">{{location.name}} - {{location.url}}</li>
+      <h1 v-for="location in locations">{{location.name}} - {{location.url}}</h1>
+      <!-- <li v-for="location in locations">{{location.name}} - {{location.url}}</li> -->
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, consequuntur facilis delectus porro accusamus debitis rerum illo blanditiis corrupti pariatur. Sint, pariatur sequi. Aperiam pariatur earum doloribus sequi! Eaque, reiciendis!</p>
  </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      title: []
-    };
-  },
-  props: ["locations", "name", "url"],
-  methods: {
-    addName() {
-      return this.title.push(location.name);
-    }
-  }
+  // data() {
+  //   return {
+  //     loc: this.locations,
+  //     name: loc.name
+  //   };
+  // },
+  props: ["locations"]
 };
 </script>
 
